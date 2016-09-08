deadbed60066864493dbe574645af9f114bce020
Tram <tramngo2@gmail.com>
2016-09-07 19:03:19 -1000

    almost completed except last one

--- calculator.js
+++ calculator.js
@@ -11,8 +11,6 @@ function calculatorModule () {
   var _memory = 0;
   var _total = 0; 
 
-
-
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
@@ -39,43 +37,69 @@ calculator.getTotal=function() {
    * @param { Number } x
    */
 
+calculator.add=function(x) {
+  _total += x;
+};
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
 
+calculator.subtract=function(x) {
+  _total -= x;
+};
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
 
+calculator.multiply=function(x) {
+  _total *=x;
+};
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+calculator.divide=function(x) {
+  _total /=x;
+};
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+calculator.recallMemory=function() {
+  return _memory;
+};
 
   /**
    * Stores the value of `total` to `memory`
    */
 
+calculator.saveMemory=function() {
+  _memory = _total;
+};
 
   /**
    * Clear the value stored at `memory`
    */
 
+calculator.clearMemory = function() {
+  _memory = 0;
+};
+
   /**
    * Validation
    */
-
-   return calculator;
+calculator.validation = function(newName){
+   if(typeof newName !== 'number'){
+     throw new Error('Wrong data type you dummy');
+   }
+  };
+  return calculator;
 }
